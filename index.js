1f329ae3192762ec64e0f6136dc265b50b1672a4
Ayomide200024 <100101156+Ayomide200024@users.noreply.github.com>
2022-06-24 02:10:29 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const about_me = { name : "Oyewole Ayomide", height : "5ft 7in", country : "Nigeria",
-};
-
-
-
-alert(
-  `My name is ${about_me.name} , I am ${about_me.height} tall and I  am from ${about_me.country}.`
-);
